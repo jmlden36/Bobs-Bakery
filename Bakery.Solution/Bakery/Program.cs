4531ff48f42bbff7c266ef21999821ab3a729901
Matt Luker <jamesmattluker@gmail.com>
2021-12-10 14:57:42 -0700

    add code to Pogram.cs take the order

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -12,7 +12,12 @@ public class Program
     Pastry pastryDeals = new Pastry(2, 3, 5);
 
     Console.WriteLine("\n\n\nWelcome to Pierre's Bakery.  Today's prices are\n===============================================\n  Bread: 1 for $" + breadDeals.RegularPrice + ".\n  Today's Bread Deal: Buy 2 get 1 free!\n\n  Pastry: 1 for $" + pastryDeals.RegularPrice + ".\n  Today's Pastry Deal: Buy 2 get the third half off!");
-    
-    
+    Console.WriteLine("\n\n\nHow many loafs of your bread would you like?\n\nPlease enter a number");
+    int breadNum = int.Parse(Console.ReadLine());
+    Console.WriteLine("\n\n\nHow many pastries would you like?\n\nPlease enter a number");
+    int pastryNum = int.Parse(Console.ReadLine());
+    Console.WriteLine("Does this order look correct?\n  " + breadNum + " loafs of bread\n  " + pastryNum + " pastries\n'Y' for yes or 'N' for no");
+    string correctOrder = (Console.ReadLine()).ToLower();
+    Console.WriteLine(correctOrder);
   }
 }
