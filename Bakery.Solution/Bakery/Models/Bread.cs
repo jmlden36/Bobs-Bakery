d3371df723e60025dd0ea9216f63195653e0caf6
Matt Luker <jamesmattluker@gmail.com>
2021-12-10 10:13:42 -0700

    add code to pass Bread Constructor test 1

--- Bakery.Solution/Bakery/Models/Bread.cs
+++ Bakery.Solution/Bakery/Models/Bread.cs
@@ -5,6 +5,15 @@ namespace Bakery.Models
 {
   public class Bread
   {
+    public int BreadDealPrice { get; set; }
+
+    public int BreadDealCount { get; set; }
+
+    public Bread(int breadDealCount, int breadDealPrice)
+    {
+      BreadDealCount = breadDealCount;
+      BreadDealPrice = breadDealPrice;      
+    }
     public static int BreadCost(int input)
     {
       int cost = input * 5;
