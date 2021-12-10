269103d56f306755af1ae17d6303713b60e68602
Matt Luker <jamesmattluker@gmail.com>
2021-12-10 15:44:11 -0700

    add CalcTotal to Program to calculate the total price for an order

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -5,7 +5,12 @@ using BakeryBread.Models;
 
 
 public class Program
-{
+{ 
+  public static int CalcTotal(int val1, int val2)
+  {
+    int total = val1 + val2;
+    return total;
+  }
   public static void Main()
   {
     Bread breadDeals = new Bread(5, 3, 10);
@@ -13,11 +18,14 @@ public class Program
 
     Console.WriteLine("\n\n\nWelcome to Pierre's Bakery.  Today's prices are\n===============================================\n  Bread: 1 for $" + breadDeals.RegularPrice + ".\n  Today's Bread Deal: Buy 2 get 1 free!\n\n  Pastry: 1 for $" + pastryDeals.RegularPrice + ".\n  Today's Pastry Deal: Buy 2 get the third half off!");
     Console.WriteLine("\n\n\nHow many loafs of your bread would you like?\n\nPlease enter a number");
-    int breadNum = int.Parse(Console.ReadLine());
+    int breadNum = int.Parse(Console.ReadLine()); 
+    
     Console.WriteLine("\n\n\nHow many pastries would you like?\n\nPlease enter a number");
     int pastryNum = int.Parse(Console.ReadLine());
+    
     Console.WriteLine("Does this order look correct?\n  " + breadNum + " loafs of bread\n  " + pastryNum + " pastries\n'Y' for yes or 'N' for no");
     string correctOrder = (Console.ReadLine()).ToLower();
     Console.WriteLine(correctOrder);
+    Console.WriteLine("Your total will be: $" + CalcTotal(breadDeals.BreadCost(breadNum), (pastryDeals.PastryCost(pastryNum))));
   }
 }
