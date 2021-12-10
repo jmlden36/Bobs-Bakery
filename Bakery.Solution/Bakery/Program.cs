23101336ddc130baac3883668a1beec06efc9dd9
Matt Luker <jamesmattluker@gmail.com>
2021-12-10 14:36:07 -0700

    add welcome message to Program.cs

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -11,6 +11,8 @@ public class Program
     Bread breadDeals = new Bread(5, 3, 10);
     Pastry pastryDeals = new Pastry(2, 3, 5);
 
-    Console.WriteLine("test");
+    Console.WriteLine("\n\n\nWelcome to Pierre's Bakery.  Today's prices are\n===============================================\n  Bread: 1 for $" + breadDeals.RegularPrice + ".\n  Today's Bread Deal: Buy 2 get 1 free!\n\n  Pastry: 1 for $" + pastryDeals.RegularPrice + ".\n  Today's Pastry Deal: Buy 2 get the third half off!");
+    
+    
   }
 }
