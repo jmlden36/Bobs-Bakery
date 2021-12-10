5760c98afa952925236af620047c4dc293da163a
Matt Luker <jamesmattluker@gmail.com>
2021-12-10 16:54:57 -0700

    fix typos in Program.cs UI text

--- Bakery.Solution/Bakery/Program.cs
+++ Bakery.Solution/Bakery/Program.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using BakeryPastry.Models;
 using BakeryBread.Models;
 
@@ -18,20 +17,19 @@ public class Program
     Pastry pastryDeals = new Pastry(2, 3, 5);
     
 
-    Console.WriteLine("\n\n\nWelcome to Pierre's Bakery.  Here are today's prices:\n=====================================================\n  Bread: 1 sourdough for $" + breadDeals.RegularPrice + "\n         1 french bread for $" + frBreadDeals.RegularPrice + "\n\n  -->Today's Bread Deal: Buy 2 get 1 free!\n\n  Pastry: 1 danish for $" + pastryDeals.RegularPrice + ".\n\n  -->Today's Pastry Deal: Buy 2 get the third half off!");
+    Console.WriteLine("\n\n\nWelcome to Pierre's Bakery.  Here are today's prices:\n=====================================================\n  Bread: 1 sourdough loaf for $" + breadDeals.RegularPrice + "\n         1 french bread loaf for $" + frBreadDeals.RegularPrice + "\n\n  -->Today's Bread Deal: Buy 2 get 1 free!\n\n  Pastry: 1 danish for $" + pastryDeals.RegularPrice + ".\n\n  -->Today's Pastry Deal: Buy 2 get the third half off!");
     
-    Console.WriteLine("\n\n\nHow many loafs of sourdough bread would you like?\n\nPlease enter a number");
+    Console.WriteLine("\n\n\nHow many loaves of sourdough bread would you like?\n\nPlease enter a number");
     int breadNum = int.Parse(Console.ReadLine()); 
     
-    Console.WriteLine("\n\n\nHow many pastries would you like?\n\nPlease enter a number");
+    Console.WriteLine("\n\n\nHow many danishes would you like?\n\nPlease enter a number");
     int pastryNum = int.Parse(Console.ReadLine());
     
-    Console.WriteLine("How many loafs of french bread would you like?\n\nPlease enter a number");
+    Console.WriteLine("\n\n\nHow many loaves of french bread would you like?\n\nPlease enter a number");
     int frBreadNum = int.Parse(Console.ReadLine());
 
-    Console.WriteLine("Does this order look correct?\n  " + breadNum + " loafs of sourdough bread\n  " + pastryNum + " pastries\n  " + frBreadNum + " loafs of french bread\n'Y' for yes or 'N' for no");
-    string correctOrder = (Console.ReadLine()).ToLower();
-    Console.WriteLine(correctOrder);
+    Console.WriteLine("\n\n\nDoes this order look correct?\n  " + breadNum + " loaves of sourdough bread\n  " + pastryNum + " danishes\n  " + frBreadNum + " loaves of french bread\n'Y' for yes or 'N' for no");
+    string correctOrder = (Console.ReadLine()).ToLower();    
     Console.WriteLine("Your total will be: $" + CalcTotal(breadDeals.BreadCost(breadNum), (pastryDeals.PastryCost(pastryNum)), (frBreadDeals.BreadCost(frBreadNum))));
   }
 }
